621e3e1e8d73881533b89d637c49883f4463a4eb
Scopophage <bonenfant.erwann@gmail.com>
2025-02-15 22:11:22 +0100

    Mise à jour du CV

--- javascript.js
+++
@@ -1,6 +0,0 @@
-// Exemple d'une interaction dynamique : afficher/masquer des informations
-document.querySelectorAll('.job').forEach(job => {
-    job.addEventListener('click', function() {
-        this.classList.toggle('expanded');
-    });
-});
